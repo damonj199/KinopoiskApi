2d418edf832971c75801a74d05b97faa3784e5d1
Dmitriy Dmitriy <dbilica19@gmail.com>
2024-10-11 15:53:08 +0300

    Add models Add data base add initial migration

---
+++ KinopoiskDB.Core/Dtos/CountriesDto.cs
@@ -0,0 +1,9 @@
+﻿namespace KinopoiskDB.Core.Dtos;
+
+public class CountriesDto
+{
+    public int Id { get; set; }
+    public string Country { get; set; }
+    public List<MoviesDto> Movies { get; set; }
+
+}
---
+++ KinopoiskDB.DAL/Repository/BaseRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace KinopoiskDB.Dal.Repository;
+
+public class BaseRepository
+{
+    protected readonly KinopoiskDbContext _cxt;
+    public BaseRepository(KinopoiskDbContext connectionString)
+    {
+        _cxt = connectionString;
+    }
+}
