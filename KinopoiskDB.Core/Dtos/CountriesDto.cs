17e82eb47b18476efc090ca0bf2addf3d1421782
Dmitry Bilitsa <dbilitsa@a2seven.com>
2024-10-15 11:41:23 +0000

    fix

--- KinopoiskDB.Core/Dtos/CountriesDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KinopoiskDB.Core.Dtos;
-
-public class CountriesDto
-{
-    public int Id { get; set; }
-    public string Country { get; set; }
-    public List<MoviesDto> Movies { get; set; }
-
-}
--- KinopoiskDB.Core/Dtos/GenresDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KinopoiskDB.Core.Dtos;
-
-public class GenresDto
-{
-    public int Id { get; set; }
-    public string Genre { get; set; }
-    public List<MoviesDto> Movies { get; set; }
-}
